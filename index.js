e2e9bcc7fe63391501743034a3adef5125c5d439
patipan-patisampita <patipan.patisampita@gmail.com>
2022-07-05 22:09:53 +0700

    update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http')
 const hostname = 'localhost'
 const fs = require('fs')
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 
 const server = http.createServer((req, res) => {
     if (req.url == '/') {
