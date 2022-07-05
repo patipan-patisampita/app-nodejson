7ebe78bfb2cb61df4a9c31ed04aa02e5b461ee5a
patipan-patisampita <patipan.patisampita@gmail.com>
2022-07-05 17:25:10 +0700

    update

--- index.js
+++ index.js
@@ -3,8 +3,17 @@ const hostname = 'localhost'
 const port = process.env.PORT || 3000
 
 const server = http.createServer((req, res) => {
-    res.write("Welcome")
-    res.end()
+    if (req.url == '/') {
+        //set response header
+        res.writeHead(200, { 'Content-Type': 'text/html' })
+        //set response content
+        res.write(`<html><body><p>This is home page.</p></body></html>`)
+        res.end()
+    } else if (req.url == '/contact') {
+        res.writeHead(200, { 'Content-Type': 'text/html' })
+        res.write(`<html><head><body><p>This is contact Page.</p></body></head></html>`)
+        res.end()
+    }
 })
 
 server.listen(port, hostname, () => {
